db75b58a7e2f8335af25dee367dfea1ba8e7dae2
Jeffrey Brekke <brekke@apache.org>
2002-04-03 04:38:40 +0000

    Moving com.oroinc.net.ftp to org.apache.commons.net.ftp
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/net/trunk@139283 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/net/ftp/FTPFileListParser.java
@@ -0,0 +1,91 @@
+package org.apache.commons.net.ftp;
+
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and
+ *    "Apache Commons" must not be used to endorse or promote products
+ *    derived from this software without prior written permission. For
+ *    written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    "Apache Turbine", nor may "Apache" appear in their name, without
+ *    prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+import java.io.*;
+
+/***
+ * FTPFileListParser defines the interface for parsing FTP file listings
+ * and converting that information into an array of
+ * <a href="org.apache.commons.net.ftp.FTPFile.html"> FTPFile </a> instances.
+ * Sometimes you will want to parse unusual listing formats, in which
+ * case you would create your own implementation of FTPFileListParser and
+ * if necessary, subclass FTPFile.
+ * <p>
+ * <p>
+ * @author Daniel F. Savarese
+ * @see FTPFile
+ * @see FTPClient#listFiles
+ ***/
+
+public interface FTPFileListParser {
+
+  /***
+   * Parses an FTP server file listing and converts it into a usable format
+   * in the form of an array of <code> FTPFile </code> instances.  If the
+   * file list contains no files, <code> null </code> should be
+   * returned, otherwise an array of <code> FTPFile </code> instances
+   * representing the files in the directory is returned.
+   * <p>
+   * @param listStream The InputStream from which the file list should be
+   *        read.
+   * @return The list of file information contained in the given path.  null
+   *     if the list could not be obtained or if there are no files in
+   *     the directory.
+   * @exception IOException  If an I/O error occurs reading the listStream.
+   ***/
+  public FTPFile[] parseFileList(InputStream listStream) throws IOException;
+
+} 
