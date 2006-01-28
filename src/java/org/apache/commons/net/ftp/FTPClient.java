6e27f355d10cc68055a5412dde99121843776de9
Rory Winston <rwinston@apache.org>
2006-01-28 17:51:49 +0000

    Close ServerSocket resource on IOException
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/net/trunk@373191 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/net/ftp/FTPClient.java
+++ src/java/org/apache/commons/net/ftp/FTPClient.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2001-2005 The Apache Software Foundation
+ * Copyright 2001-2006 The Apache Software Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -499,7 +499,12 @@ implements Configurable
             // to allow preventing the accept from blocking indefinitely.
             if (__dataTimeout >= 0)
                 server.setSoTimeout(__dataTimeout);
-            socket = server.accept();
+            try {
+				socket = server.accept();
+			} catch (IOException ioe) {
+				server.close();
+				throw ioe;
+			}
             server.close();
         }
         else
