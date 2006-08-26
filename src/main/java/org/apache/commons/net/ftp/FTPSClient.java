dcea3bb4343dcf1b818fd7412ea0adefa30ebbf5
Rory Winston <rwinston@apache.org>
2006-08-26 09:17:06 +0000

    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/net/branches/JDK_1_5_BRANCH@437131 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/net/ftp/FTPSClient.java
+++ src/main/java/org/apache/commons/net/ftp/FTPSClient.java
@@ -30,27 +30,62 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.TrustManager;
 
-public class FTPSClient extends FTPClient
-{
-	private static final String PASSWORD = "password";
+
+/**
+ * 
+ * This class extends {@link org.apache.commons.net.ftp.FTPClient} to add
+ * the necessary methods that implement SSL/TLS-FTPS.
+ *
+ */
+public class FTPSClient extends FTPClient {
+
+	// Represent the method to the FTP command AUTH...
+	private String sslContext;
 	
+	// Secure context (can be "TLS" or "SSL")
 	private SSLContext context;
 	
-	public FTPSClient()
-	{
-		try
-		{
-			KeyStore keyStore = KeyStore.getInstance("JCEKS");
-			keyStore.load(null, PASSWORD.toCharArray());
+	private String pbsz;
+	private String prot;
+
+	/**
+	 * Default constructor that selects some default options (TLS encryption)
+	 *
+	 */
+	public FTPSClient() {
+		this("JCEKS", "TLS", "password", "0", "P");
+	}
+	
+	
+	/**
+	 * 
+	 * Constructor that initializes the secure connection. 
+	 * 
+	 * @param keyStoreName Type of instance KeyStore, JKS for Java 1.3 y JCEKS for Java 1.4 
+	 * @param sslContext Type of the instance SSLContext, can be SSL or TLS.
+	 * @param password The password to access the KeyStore.
+	 */
+	public FTPSClient(String keyStoreName, String sslContext, String password, String pbsz, String prot) {
+		this.sslContext = sslContext;
+		this.pbsz = pbsz;
+		this.prot = prot;
+		
+		try {
+			KeyStore keyStore = KeyStore.getInstance(keyStoreName);
+			
+			keyStore.load(null, password.toCharArray());
 
 			KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-			keyManagerFactory.init(keyStore, PASSWORD.toCharArray());
+			
+			keyManagerFactory.init(keyStore, password.toCharArray());
 
-			this.context = SSLContext.getInstance("TLS");
-			this.context.init(keyManagerFactory.getKeyManagers(), new TrustManager[] { new FTPSTrustManager() }, null);
-		}
-		catch (Exception e)
-		{
+			this.context = SSLContext.getInstance(sslContext);
+
+			this.context.init(
+				keyManagerFactory.getKeyManagers(), 
+				new TrustManager[] { (TrustManager) new FTPSTrustManager() }, null
+			);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
@@ -62,7 +97,7 @@ public class FTPSClient extends FTPClient
 	{
 		super.connect(address, port, localAddress, localPort);
 		
-		this.secure();
+		this.secure(this.pbsz,this.prot);
 	}
 
 	/**
@@ -72,7 +107,7 @@ public class FTPSClient extends FTPClient
 	{
 		super.connect(address, port);
 		
-		this.secure();
+		this.secure(this.pbsz,this.prot);
 	}
 
 	/**
@@ -82,7 +117,7 @@ public class FTPSClient extends FTPClient
 	{
 		super.connect(address, port, localAddress, localPort);
 		
-		this.secure();
+		this.secure(this.pbsz,this.prot);
 	}
 
 	/**
@@ -92,39 +127,54 @@ public class FTPSClient extends FTPClient
 	{
 		super.connect(address, port);
 		
-		this.secure();
+		this.secure(this.pbsz,this.prot);
 	}
 	
-	public void secure() throws IOException
-	{
-		this.sendCommand("AUTH", "TLS");
+	/**
+	 *
+	 * Initialize the secure connection with the FTP server, throw the AUTH SSL o TLS command.
+	 * Get the socket with the server, starting the "handshake" making the socket, with a layer of securety,
+	 * and initializing the stream of connection.
+	 * 
+	 * 
+	 * @param pbsz Protection Buffer Size: "0" is a good value
+	 * @param prot Data Channel Protection Level:
+	 * Posible values:
+	 * C - Clear
+	 * S - Safe
+	 * E - Confidential 
+	 * P - PrivateType of secure connection
+	 *  
+	 * @throws IOException If there is any problem with the connection.
+	 */
+	protected void secure(String pbsz, String prot) throws IOException {
+		this.sendCommand("AUTH", sslContext);
 		
-		SSLSocket socket = (SSLSocket) this.context.getSocketFactory().createSocket(this._socket_, this.getRemoteAddress().getHostAddress(), this.getRemotePort(), true);
+		SSLSocket socket = (SSLSocket)this.context.getSocketFactory().createSocket(this._socket_, this.getRemoteAddress().getHostAddress(), this.getRemotePort(), true);
 		
 		socket.startHandshake();
-		
+
 		this._socket_ = socket;
+		
 		this._controlInput_ = new BufferedReader(new InputStreamReader(socket.getInputStream(), getControlEncoding()));
 		this._controlOutput_ = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), getControlEncoding()));
 
 		this.setSocketFactory(new FTPSSocketFactory(this.context));
-		
-		this.sendCommand("PBSZ", "0");
-		this.sendCommand("PROT", "P");
+
+		this.sendCommand("PBSZ", pbsz);
+		this.sendCommand("PROT", prot);
 	}
 
 	/**
-	 * @see org.apache.commons.net.ftp.FTPClient#_openDataConnection_(int, java.lang.String)
-	 */
-	protected Socket _openDataConnection_(int command, String arg) throws IOException
-	{
+	 * @see org.apache.commons.net.ftp.FTPCliente#_openDataConnection_(java.lang.String, int)
+	 */	
+	protected Socket _openDataConnection_(int command, String arg) throws IOException {
 		Socket socket = super._openDataConnection_(command, arg);
-		
-		if (socket != null)
-		{
-			((SSLSocket) socket).startHandshake();
+		if (socket != null) {
+			((SSLSocket)socket).startHandshake();
 		}
-		
 		return socket;
-	}
+	}	
+
 }
+
