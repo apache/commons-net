30f5293c15477339ca1f6e5ba60a17da24e08000
Rory Winston <rwinston@apache.org>
2008-03-08 19:49:53 +0000

    Add a fix for UNKNOWN Type: L8 SYST messages (NET-169)
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/net/branches/NET_2_0@635059 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/net/ftp/parser/DefaultFTPFileEntryParserFactory.java
+++ src/main/java/org/apache/commons/net/ftp/parser/DefaultFTPFileEntryParserFactory.java
@@ -97,7 +97,8 @@ public class DefaultFTPFileEntryParserFactory
 	            {
 	                ukey = key.toUpperCase();
 	            }
-	            if (ukey.indexOf(FTPClientConfig.SYST_UNIX) >= 0)
+	            if ((ukey.indexOf(FTPClientConfig.SYST_UNIX) >= 0) 
+	            		|| (ukey.indexOf(FTPClientConfig.SYST_L8) >= 0))
 	            {
 	                parser = createUnixFTPEntryParser();
 	            }
