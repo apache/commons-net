2934c3f8fb0f2ba1290121ab13a7608e98aaa86e
Rory Winston <rwinston@apache.org>
2008-03-09 17:33:39 +0000

    Remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/net/branches/NET_2_0@635309 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/net/ftp/parser/FTPTimestampParserImpl.java
+++ src/main/java/org/apache/commons/net/ftp/parser/FTPTimestampParserImpl.java
@@ -23,7 +23,6 @@ import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
 import org.apache.commons.net.ftp.Configurable;
