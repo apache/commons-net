e9541b11d57abab15cd7d53a8d769702467793b9
Sebastian Bazley <sebb@apache.org>
2009-02-27 02:06:45 +0000

    Unused import
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/net/branches/NET_2_0@748377 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/net/ftp/FTP.java
+++ src/main/java/org/apache/commons/net/ftp/FTP.java
@@ -25,7 +25,6 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.SocketException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import org.apache.commons.net.MalformedServerReplyException;
 import org.apache.commons.net.ProtocolCommandListener;
