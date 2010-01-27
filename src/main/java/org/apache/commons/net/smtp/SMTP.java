4bd743e281996dcd10b9a45660c9e98d60bef2fe
Sebastian Bazley <sebb@apache.org>
2010-01-27 21:17:21 +0000

    Unused import
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/net/branches/NET_2_0@903843 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/net/smtp/SMTP.java
+++ src/main/java/org/apache/commons/net/smtp/SMTP.java
@@ -23,7 +23,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import org.apache.commons.net.MalformedServerReplyException;
 import org.apache.commons.net.ProtocolCommandListener;
