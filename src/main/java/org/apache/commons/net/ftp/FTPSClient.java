366a10b214a796dc8d75ea74dfd227ffdf585e9b
Sebastian Bazley <sebb@apache.org>
2011-03-02 19:51:27 +0000

    Removed deprecated unused fields from FTPSClient.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/net/trunk@1076354 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/net/ftp/FTPSClient.java
+++ src/main/java/org/apache/commons/net/ftp/FTPSClient.java
@@ -41,22 +41,6 @@ import javax.net.ssl.TrustManager;
  * @since 2.0
  */
 public class FTPSClient extends FTPClient {
-
-    /** @deprecated - not used - will be removed in next major release */
-    @Deprecated
-    public static String KEYSTORE_ALGORITHM;
-
-    /** @deprecated - not used - will be removed in next major release */
-    @Deprecated
-    public static String TRUSTSTORE_ALGORITHM;
-
-    /** @deprecated - not used - will be removed in next major release */
-    @Deprecated
-    public static String PROVIDER;
-
-    /** @deprecated - not used - will be removed in next major release */
-    @Deprecated
-    public static String STORE_TYPE;
     
     /** The value that I can set in PROT command  (C = Clear, P = Protected) */
     private static final String[] PROT_COMMAND_VALUE = {"C","E","S","P"};
