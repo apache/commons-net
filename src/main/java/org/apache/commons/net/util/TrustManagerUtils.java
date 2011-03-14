ab587439f95411545a51886aedc068a5b0548231
Sebastian Bazley <sebb@apache.org>
2011-03-14 16:14:25 +0000

    Rename class to avoid clashing with javax version Add method to generate default TrustManager
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/net/trunk@1081438 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/net/util/TrustManagerUtils.java
+++ src/main/java/org/apache/commons/net/util/TrustManagerUtils.java
@@ -17,27 +17,30 @@
 
 package org.apache.commons.net.util;
 
+import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
+import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
 /**
- * TrustManager factory for generating custom TrustManagers.
- * 
+ * TrustManager utilities for generating TrustManagers.
+ *
  * @since 3.0
  */
-public final class TrustManagerFactory
+public final class TrustManagerUtils
 {
     private static final X509Certificate[] EMPTY_X509CERTIFICATE_ARRAY = new X509Certificate[]{};
 
     private static class TrustManager implements X509TrustManager {
 
         private final boolean checkServerValidity;
-        
+
         TrustManager(boolean checkServerValidity) {
             this.checkServerValidity = checkServerValidity;
         }
+
         /**
          * Never generates a CertificateException.
          */
@@ -46,7 +49,8 @@ public final class TrustManagerFactory
             return;
         }
 
-        public void checkServerTrusted(X509Certificate[] certificates, String authType) throws CertificateException
+        public void checkServerTrusted(X509Certificate[] certificates, String authType)
+            throws CertificateException
         {
             if (checkServerValidity) {
                 for (int i = 0; i < certificates.length; ++i)
@@ -62,16 +66,16 @@ public final class TrustManagerFactory
         public X509Certificate[] getAcceptedIssuers()
         {
             return EMPTY_X509CERTIFICATE_ARRAY;
-        }        
+        }
     }
 
     private static final X509TrustManager ACCEPT_ALL=new TrustManager(false);
-    
+
     private static final X509TrustManager CHECK_SERVER_VALIDITY=new TrustManager(true);
 
     /**
      * Generate a TrustManager that performs no checks.
-     * 
+     *
      * @return the TrustManager
      */
     public static X509TrustManager getAcceptAllTrustManager(){
@@ -81,11 +85,24 @@ public final class TrustManagerFactory
     /**
      * Generate a TrustManager that checks server certificates for validity,
      * but otherwise performs no checks.
-     * 
+     *
      * @return the validating TrustManager
      */
     public static X509TrustManager getValidateServerCertificateTrustManager(){
-        return CHECK_SERVER_VALIDITY;        
+        return CHECK_SERVER_VALIDITY;
+    }
+
+    /**
+     * Return the default TrustManager provided by the JVM.
+     * <p>
+     * This should be the same as the default used by {@link javax.net.ssl.SSLContext#init(javax.net.ssl.KeyManager[], javax.net.ssl.TrustManager[], java.security.SecureRandom)
+     * SSLContext#init(KeyManager[], TrustManager[], SecureRandom)}
+     * when the TrustManager parameter is set to {@code null}
+     * @return the default TrustManager
+     * @throws NoSuchAlgorithmException
+     */
+    public static X509TrustManager getDefaultTrustManager() throws NoSuchAlgorithmException {
+        return (X509TrustManager) TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
     }
 
 }
