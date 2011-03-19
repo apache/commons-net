cde2efc78463a4ffeefb92974c573b540f0e421e
Sebastian Bazley <sebb@apache.org>
2011-03-19 16:20:23 +0000

    Override preParse() as not needed Fix up some parsing errors, add sizd (size of directory) handling
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/net/trunk@1083209 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/net/ftp/parser/MLSxEntryParser.java
+++ src/main/java/org/apache/commons/net/ftp/parser/MLSxEntryParser.java
@@ -20,6 +20,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.TimeZone;
 
@@ -84,6 +85,15 @@ public class MLSxEntryParser extends FTPFileEntryParserImpl
         super();
     }
 
+    /**
+     * Override preParse() phase as it is not needed.
+     */
+    // TODO can be removed if NET-381 is implemented
+    @Override
+    public List<String> preParse(List<String> orig) {
+        return orig;
+    }
+
     public FTPFile parseFTPEntry(String entry) {
         String parts[] = entry.split(" ",2); // Path may contain space
         if (parts.length != 2) {
@@ -96,9 +106,18 @@ public class MLSxEntryParser extends FTPFileEntryParserImpl
         boolean hasUnixMode = parts[0].toLowerCase(Locale.ENGLISH).contains("unix.mode=");
         for(String fact : facts) {
             String []factparts = fact.split("=");
+// Sample missing permission
+// drwx------   2 mirror   mirror       4096 Mar 13  2010 subversion
+// modify=20100313224553;perm=;type=dir;unique=811U282598;UNIX.group=500;UNIX.mode=0700;UNIX.owner=500; subversion
+            if (factparts.length != 2) {
+                continue; // nothing to do here
+            }
             String factname = factparts[0].toLowerCase(Locale.ENGLISH);
             String factvalue = factparts[1];
             if ("size".equals(factname)) {
+                file.setSize(Long.parseLong(factvalue));
+            }
+            else if ("sizd".equals(factname)) { // Directory size
                 file.setSize(Long.parseLong(factvalue));
             }
             else if ("modify".equals(factname)) {
@@ -127,9 +146,10 @@ public class MLSxEntryParser extends FTPFileEntryParserImpl
                     file.setGroup(factvalue);
                 } else if ("owner".equals(unixfact)){
                     file.setUser(factvalue);
-                } else if ("mode".equals(unixfact)){ // e.g. 0755
+                } else if ("mode".equals(unixfact)){ // e.g. 0[1]755
+                    int off = factvalue.length()-4; // only parse last 3 digits
                     for(int i=1; i<=3; i++){
-                        int ch = factvalue.charAt(i)-'0';
+                        int ch = factvalue.charAt(off+i)-'0';
                         for(int p : UNIX_PERMS[ch]) {
                             file.setPermission(UNIX_GROUPS[i-1], p, true);
                         }
