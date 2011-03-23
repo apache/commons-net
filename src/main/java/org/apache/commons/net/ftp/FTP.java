apache/commons-net

commit 5b366f24f6169676c625bcdc2b5ca87aae13f915

NET-385 FTP does not apply timeout to initial responses
git-svn-id: https://svn.apache.org/repos/asf/commons/proper/net/trunk@1083955 13f79535-47bb-0310-9956-ffa450edef68

src/main/java/org/apache/commons/net/ftp/FTP.java
@@ -25,6 +25,7 @@ import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 
 import org.apache.commons.net.MalformedServerReplyException;
@@ -375,10 +376,22 @@ public class FTP extends SocketClient
             new BufferedReader(new InputStreamReader(_input_, getControlEncoding()));
         _controlOutput_ =
             new BufferedWriter(new OutputStreamWriter(_output_, getControlEncoding()));
-        __getReply();
-        // If we received code 120, we have to fetch completion reply.
-        if (FTPReply.isPositivePreliminary(_replyCode))
-            __getReply();
+        if (connectTimeout > 0) { // NET-385
+            int original = _socket_.getSoTimeout();
+            _socket_.setSoTimeout(connectTimeout); 
+            try {
+                __getReply();
+                // If we received code 120, we have to fetch completion reply.
+                if (FTPReply.isPositivePreliminary(_replyCode))
+                    __getReply();
+            } catch (SocketTimeoutException e) {
+                IOException ioe = new IOException("Timed out waiting for initial connect reply");
+                ioe.initCause(e);
+                throw ioe;
+            } finally {
+                _socket_.setSoTimeout(original);
+            }
+        }
     }
 
 

commit 3097e3a539b5ddcd0ba8d3c8b1bcd296d10b41c6

Fix bug introduced in r1083955 to fix NET-385 - don't skip initial reply if not applying timeout (oops)
git-svn-id: https://svn.apache.org/repos/asf/commons/proper/net/trunk@1084622 13f79535-47bb-0310-9956-ffa450edef68

src/main/java/org/apache/commons/net/ftp/FTP.java
@@ -378,7 +378,7 @@ public class FTP extends SocketClient
             new BufferedWriter(new OutputStreamWriter(_output_, getControlEncoding()));
         if (connectTimeout > 0) { // NET-385
             int original = _socket_.getSoTimeout();
-            _socket_.setSoTimeout(connectTimeout); 
+            _socket_.setSoTimeout(connectTimeout);
             try {
                 __getReply();
                 // If we received code 120, we have to fetch completion reply.
@@ -391,6 +391,11 @@ public class FTP extends SocketClient
             } finally {
                 _socket_.setSoTimeout(original);
             }
+        } else {
+            __getReply();
+            // If we received code 120, we have to fetch completion reply.
+            if (FTPReply.isPositivePreliminary(_replyCode))
+                __getReply();            
         }
     }
 