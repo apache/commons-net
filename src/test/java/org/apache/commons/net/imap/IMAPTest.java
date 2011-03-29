8a158ff2e7ab57dbc8471533276f76d86389c494
Sebastian Bazley <sebb@apache.org>
2011-03-29 15:57:29 +0000

    COmment and update test
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/net/trunk@1086635 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/net/imap/IMAPTest.java
+++ src/test/java/org/apache/commons/net/imap/IMAPTest.java
@@ -27,6 +27,9 @@ public class IMAPTest {
 
     @Test
     public void checkGenerator() {
+        // This test assumes:
+        // - 26 letters in the generator alphabet
+        // - the generator uses a fixed size tag
         IMAP imap = new IMAP();
         String initial = imap.generateCommandID();
         int expected = 1;
@@ -34,16 +37,16 @@ public class IMAPTest {
             expected *= 26; // letters in alphabet
         }
         int i=0;
-        while(true) {
+        boolean matched=false;
+        while(i <= expected+10) { // don't loop forever, but allow it to pass go!
             i++;
             String s = imap.generateCommandID();
-            if (initial.equals(s)) { // we've wrapped around completely
-                break;
-            }
-            if (i > expected) {
+            matched = initial.equals(s);
+            if (matched) { // we've wrapped around completely
                 break;
             }
         }
         Assert.assertEquals(expected, i);
+        Assert.assertTrue("Expected to see the original value again", matched);
     }
 }
