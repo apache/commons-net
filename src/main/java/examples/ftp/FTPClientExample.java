f7ae4e1bec8640a902aaf5d0f3ec23a704735409
Sebastian Bazley <sebb@apache.org>
2013-02-01 21:25:19 +0000

    Tweak help message
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/net/trunk@1441639 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/examples/ftp/FTPClientExample.java
+++ src/main/java/examples/ftp/FTPClientExample.java
@@ -54,7 +54,7 @@ public final class FTPClientExample
         "Usage: ftp [options] <hostname> <username> <password> [<remote file> [<local file>]]\n" +
         "\nDefault behavior is to download a file and use ASCII transfer mode.\n" +
         "\t-a - use local active mode (default is local passive)\n" +
-        "\t-A - anonymous login\n" +
+        "\t-A - anonymous login (omit username and password parameters)\n" +
         "\t-b - use binary transfer mode\n" +
         "\t-c cmd - issue arbitrary command (remote is used as a parameter if provided) \n" +
         "\t-d - list directory details using MLSD (remote is used as the pathname if provided)\n" +
@@ -63,6 +63,7 @@ public final class FTPClientExample
         "\t-h - list hidden files (applies to -l and -n only)\n" +
         "\t-k secs - use keep-alive timer (setControlKeepAliveTimeout)\n" +
         "\t-l - list files using LIST (remote is used as the pathname if provided)\n" +
+        "\t     Files are listed twice: first in raw mode, then as the formatted parsed data.\n" +
         "\t-L - use lenient future dates (server dates may be up to 1 day into future)\n" +
         "\t-n - list file names using NLST (remote is used as the pathname if provided)\n" +
         "\t-p true|false|protocol[,true|false] - use FTPSClient with the specified protocol and/or isImplicit setting\n" +
