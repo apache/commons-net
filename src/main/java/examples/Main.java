0a3c3449fe484d17b04516d562226337c1b10255
Sebastian Bazley <sebb@apache.org>
2015-01-15 23:09:20 +0000

    Simplify examples/Main - use predefined properties file to define aliases
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/net/trunk@1652307 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/examples/Main.java
+++ src/main/java/examples/Main.java
@@ -18,15 +18,12 @@
 
 package examples;
 
-import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.security.CodeSource;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
 
 public class Main {
 
@@ -43,46 +40,31 @@ public class Main {
      * @throws Exception
      */
     public static void main(String[] args) throws Throwable  {
-        CodeSource codeSource = Main.class.getProtectionDomain().getCodeSource();
-        Map<String, String> map = new HashMap<String, String>();
-        final boolean noArgsProvided = args.length == 0;
-        if ( codeSource != null) {
-            final String sourceFile = codeSource.getLocation().getFile();
-            if (sourceFile.endsWith(".jar")) {
-                if (noArgsProvided) {
-                    System.out.println("Usage: java -jar commons-net-examples-m.n.jar <exampleClass> <exampleClass parameters>");
-                    System.out.println("\nClasses found in the jar:");
-                }
-                JarFile jf = new JarFile(sourceFile);
-                Enumeration<JarEntry> e = jf.entries();
-                while (e.hasMoreElements()) {
-                  JarEntry je = e.nextElement();
-                  String name = je.getName();
-                  processFileName(name, map, noArgsProvided);
-                }
-                jf.close();
+        final Properties fp = new Properties();
+        fp.load(Main.class.getResourceAsStream("examples.properties"));
+        if (args.length == 0) {
+            if (Thread.currentThread().getStackTrace().length > 2) { // called by Maven
+                System.out.println("Usage: mvn -q exec:java  -Dexec.arguments=<alias or exampleClass>,<exampleClass parameters> (comma-separated, no spaces)");
+                System.out.println("Or   : mvn -q exec:java  -Dexec.args=\"<alias or exampleClass> <exampleClass parameters>\" (space separated)");
             } else {
-                if (noArgsProvided) {
-                    System.out.println("Usage: mvn -q exec:java  -Dexec.arguments=<exampleClass>,<exampleClass parameters>");
-                    System.out.println("\nClasses found in the jar:");
-                }
-                File examples = new File(sourceFile, "examples");
-                if (examples.exists()) {
-                    scanForClasses(sourceFile.length(), examples, map, noArgsProvided);
+                if (fromJar()) { 
+                    System.out.println("Usage: java -jar commons-net-examples-m.n.jar <alias or exampleClass> <exampleClass parameters>");                
+                } else {
+                    System.out.println("Usage: java -cp target/classes examples/Main <alias or exampleClass> <exampleClass parameters>");                
                 }
             }
-        } else {
-            if (noArgsProvided) {
-                System.out.println("Usage: java -jar commons-net-examples-m.n.jar <exampleClass> <exampleClass parameters>");
+            System.out.println("\nAliases and their classes:");
+            @SuppressWarnings("unchecked") // property names are Strings
+            List<String> l = (List<String>) Collections.list(fp.propertyNames());
+            Collections.sort(l);
+            for(String s : l) {
+                System.out.printf("%-25s %s%n",s,fp.getProperty(s));
             }
-        }
-
-        if (noArgsProvided) {
             return;
         }
 
         String shortName = args[0];
-        String fullName = map.get(shortName);
+        String fullName = fp.getProperty(shortName);
         if (fullName == null) {
             fullName = shortName;
         }
@@ -107,36 +89,11 @@ public class Main {
         }
     }
 
-    private static void scanForClasses(int rootLength, File current, Map<String, String> map, boolean printAlias) {
-        for(File file : current.listFiles()) {
-            if (file.isDirectory()) {
-                scanForClasses(rootLength, file, map, printAlias);
-            } else {
-                
-                processFileName(file.getPath().substring(rootLength), map, printAlias);
-                
-            }
+    private static boolean fromJar() {
+        final CodeSource codeSource = Main.class.getProtectionDomain().getCodeSource();
+        if ( codeSource != null) {
+            return codeSource.getLocation().getFile().endsWith(".jar");
         }
-        
-    }
-
-    private static void processFileName(String name, Map<String, String> map, boolean printAlias) {
-        if (!name.endsWith(".class")
-                || name.contains("$") // subclasses
-                // TODO use reflection to eliminate non-main classes?
-                // however that would entail loading the class.
-                || name.equals("examples/nntp/NNTPUtils.class") // no main class
-                || name.equals("examples/util/IOUtil.class") // no main class
-                || name.equals("examples/mail/IMAPUtils.class") // no main class
-                || name.equals("examples/Main.class")) { // ourself
-            return;
-        }
-        name = name.replace(".class", "");
-        final int lastSep = name.lastIndexOf('/');
-        final String alias = name.substring(lastSep+1);
-        if (printAlias) {
-            System.out.printf("%-25s %s%n",alias,name);
-        }
-        map.put(alias, name);
+        return false; // No idea if this can happen
     }
 }
---
+++ src/test/java/examples/MainTest.java
@@ -0,0 +1,102 @@
+package examples;
+
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.CodeSource;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void checkExamplesPropertiesIsComplete() throws Exception {
+        Properties cp = scanClasses();
+        Properties fp = new Properties();
+        fp.load(this.getClass().getResourceAsStream("examples.properties"));
+        for(String c : cp.stringPropertyNames()) {
+            String fv = fp.getProperty(c); 
+            final String cv = cp.getProperty(c);
+            if (fv == null) {
+                System.out.printf("%-25s %s - missing from examples.properties%n",c,cv);
+            } else if (!fv.equals(cv)) {
+                System.out.printf("%-25s %s - expected value %s %n",c,fv,cv);
+            }
+        }
+    }
+
+    private Properties scanClasses() throws IOException {
+        CodeSource codeSource = Main.class.getProtectionDomain().getCodeSource();
+        final String sourceFile = codeSource.getLocation().getFile();
+        Properties p = new Properties();
+        if (sourceFile.endsWith(".jar")) {
+            JarFile jf = new JarFile(sourceFile);
+            Enumeration<JarEntry> e = jf.entries();
+            while (e.hasMoreElements()) {
+              JarEntry je = e.nextElement();
+              String name = je.getName();
+              processFileName(name, p);
+            }
+            jf.close();
+        } else {
+            File examples = new File(sourceFile, "examples"); // must match top level examples package name
+            if (examples.exists()) {
+                scanForClasses(sourceFile.length(), examples, p);
+            } else {
+                fail("Could not find examples classes");
+            }
+        }
+        return p;
+    }
+
+    private static void scanForClasses(int rootLength, File current, Properties p) {
+        for(File file : current.listFiles()) {
+            if (file.isDirectory()) {
+                scanForClasses(rootLength, file, p);
+            } else {                
+                processFileName(file.getPath().substring(rootLength), p);
+            }
+        }        
+    }
+
+    private static void processFileName(String name, Properties p) {
+        if (!name.endsWith(".class")
+                || name.contains("$") // subclasses
+                || name.equals("examples/Main.class")  // the initial class, don't want to add that
+                || !hasMainMethod(name)
+                ) {
+            return;
+        }
+        name = name.replace(".class", "");
+        final int lastSep = name.lastIndexOf('/');
+        final String alias = name.substring(lastSep+1);
+        if (p.containsKey(alias)) {
+            System.out.printf("Duplicate alias: %-25s %s %s %n",alias,name,p.getProperty(alias));            
+        } else {
+            p.setProperty(alias, name);
+        }
+    }
+
+    private static boolean hasMainMethod(String name) {
+        name = name.replace(".class", "");
+        name = name.replace("/", ".");
+        try {
+            Class<?> clazz = Class.forName(name, false, MainTest.class.getClassLoader());
+            clazz.getMethod("main", new Class[]{String[].class});
+            return true;
+        } catch (ClassNotFoundException e) {
+            System.out.println("Cannot find " + name);
+            return false;
+        } catch (NoSuchMethodException e) {
+            return false;
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+}
