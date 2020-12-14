6187d016521f8b1783a92d25e44e0cd837fdc26d
Gary Gregory <garydgregory@gmail.com>
2020-12-14 15:44:51 -0500

    Javadoc and formatting tweaks.

--- src/main/java/org/apache/commons/net/ftp/FTPFileFilter.java
+++ src/main/java/org/apache/commons/net/ftp/FTPFileFilter.java
@@ -19,15 +19,17 @@
 package org.apache.commons.net.ftp;
 
 /**
- * Perform filtering on FTPFile entries.
+ * Performs filtering on {@link FTPFile} instances.
+ *
  * @since 2.2
  */
 public interface FTPFileFilter {
+
     /**
      * Checks if an FTPFile entry should be included or not.
      *
-     * @param file entry to be checked for inclusion. May be <code>null</code>.
-     * @return <code>true</code> if the file is to be included, <code>false</code> otherwise
+     * @param file entry to be checked for inclusion. May be {@code null}.
+     * @return {@code true} if the file is to be included, {@code false} otherwise.
      */
     boolean accept(FTPFile file);
 }
