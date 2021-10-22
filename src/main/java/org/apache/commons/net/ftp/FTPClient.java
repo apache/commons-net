c9f1179dd95c642aab01ae23b8f1262d73f58b25
Yani Mihaylov <edgesfit@gmail.com>
2021-10-22 10:19:29 +0300

    NET-642 using execPROT on FTPSClients with Proxy Settings removes Proxy Settings do not reset proxy settings when re-setting the socket factory create method identical to open _openDataConnection_ for FTPS where proxy is used and ssl socket is created from ssl context

--- src/main/java/org/apache/commons/net/ftp/FTPClient.java
+++ src/main/java/org/apache/commons/net/ftp/FTPClient.java
@@ -1682,7 +1682,7 @@ public class FTPClient extends FTP implements Configurable {
      *
      * @return The client port for active mode.
      */
-    private int getActivePort()
+    protected int getActivePort()
     {
         if (activeMinPort > 0 && activeMaxPort >= activeMinPort)
         {
@@ -1848,7 +1848,7 @@ public class FTPClient extends FTP implements Configurable {
      * @return __activeExternalHost if non-null, else getLocalAddress()
      * @see #setActiveExternalIPAddress(String)
      */
-    private InetAddress getHostAddress()
+    protected InetAddress getHostAddress()
     {
         if (activeExternalHost != null)
         {
@@ -1969,7 +1969,7 @@ public class FTPClient extends FTP implements Configurable {
      *
      * @return __reportActiveExternalHost if non-null, else getHostAddress();
      */
-    private InetAddress getReportHostAddress() {
+    protected InetAddress getReportHostAddress() {
         if (reportActiveExternalHost != null) {
             return reportActiveExternalHost ;
         }
