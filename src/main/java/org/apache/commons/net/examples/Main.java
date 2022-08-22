5d845c01ae940aed55fb5635ba5166b42b9785c4
Sebb <sebb@apache.org>
2022-08-23 00:54:38 +0100

    Checkstyle again
    [skip ci]

--- src/main/java/org/apache/commons/net/examples/Main.java
+++ src/main/java/org/apache/commons/net/examples/Main.java
@@ -67,7 +67,7 @@ public class Main {
                 System.out.println(
                     "Usage: java -jar commons-net-examples-m.n.jar <alias or exampleClass> <exampleClass parameters>");
             } else {
-                System.out.println("Usage: java -cp target/classes org.apache.commons.net.examples.Main" + 
+                System.out.println("Usage: java -cp target/classes org.apache.commons.net.examples.Main" +
                                    " <alias or exampleClass> <exampleClass parameters>");
             }
             @SuppressWarnings("unchecked") // property names are Strings
