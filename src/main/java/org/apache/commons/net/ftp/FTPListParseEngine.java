eeb0689890a878c9e25a79a1a94c9c4f5680734e
Gary Gregory <garydgregory@gmail.com>
2022-08-30 16:55:29 -0400

    Remove unused import

--- src/main/java/org/apache/commons/net/ftp/FTPListParseEngine.java
+++ src/main/java/org/apache/commons/net/ftp/FTPListParseEngine.java
@@ -21,7 +21,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
