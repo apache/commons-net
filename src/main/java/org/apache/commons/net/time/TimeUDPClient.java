355f4f205b27b3b554cd2a19c7dd7bb28cf78671
Gary Gregory <garydgregory@gmail.com>
2023-06-24 19:10:28 -0400

    Declare and initialize local variables at the same time

--- src/main/java/org/apache/commons/net/time/TimeUDPClient.java
+++ src/main/java/org/apache/commons/net/time/TimeUDPClient.java
@@ -91,17 +91,13 @@ public final class TimeUDPClient extends DatagramSocketClient {
      * @throws IOException If an error occurs while retrieving the time.
      */
     public long getTime(final InetAddress host, final int port) throws IOException {
-        long time;
-        final DatagramPacket sendPacket;
-        final DatagramPacket receivePacket;
-
-        sendPacket = new DatagramPacket(dummyData, dummyData.length, host, port);
-        receivePacket = new DatagramPacket(timeData, timeData.length);
+        final DatagramPacket sendPacket = new DatagramPacket(dummyData, dummyData.length, host, port);
+        final DatagramPacket receivePacket = new DatagramPacket(timeData, timeData.length);
 
         checkOpen().send(sendPacket);
         checkOpen().receive(receivePacket);
 
-        time = 0L;
+        long time = 0L;
         time |= (((timeData[0] & 0xff) << 24) & 0xffffffffL);
         time |= (((timeData[1] & 0xff) << 16) & 0xffffffffL);
         time |= (((timeData[2] & 0xff) << 8) & 0xffffffffL);
