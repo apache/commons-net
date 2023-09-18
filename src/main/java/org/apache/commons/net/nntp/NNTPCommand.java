c3aa22c8176d0974e1b2f5261ad09c8e89c98251
Gary Gregory <garydgregory@gmail.com>
2023-09-18 11:21:38 -0400

    Javadoc

--- src/main/java/org/apache/commons/net/nntp/NNTPCommand.java
+++ src/main/java/org/apache/commons/net/nntp/NNTPCommand.java
@@ -20,34 +20,67 @@ package org.apache.commons.net.nntp;
 /**
  * NNTPCommand stores a set of constants for NNTP command codes. To interpret the meaning of the codes, familiarity with RFC 977 is assumed.
  */
-
 public final class NNTPCommand {
 
+    /** NNTP command code {@value}. */
     public static final int ARTICLE = 0;
+
+    /** NNTP command code {@value}. */
     public static final int BODY = 1;
+
+    /** NNTP command code {@value}. */
     public static final int GROUP = 2;
+
+    /** NNTP command code {@value}. */
     public static final int HEAD = 3;
+
+    /** NNTP command code {@value}. */
     public static final int HELP = 4;
+
+    /** NNTP command code {@value}. */
     public static final int IHAVE = 5;
+
+    /** NNTP command code {@value}. */
     public static final int LAST = 6;
+
+    /** NNTP command code {@value}. */
     public static final int LIST = 7;
+
+    /** NNTP command code {@value}. */
     public static final int NEWGROUPS = 8;
+
+    /** NNTP command code {@value}. */
     public static final int NEWNEWS = 9;
+
+    /** NNTP command code {@value}. */
     public static final int NEXT = 10;
+
+    /** NNTP command code {@value}. */
     public static final int POST = 11;
+
+    /** NNTP command code {@value}. */
     public static final int QUIT = 12;
+
+    /** NNTP command code {@value}. */
     public static final int SLAVE = 13;
+
+    /** NNTP command code {@value}. */
     public static final int STAT = 14;
+
+    /** NNTP command code {@value}. */
     public static final int AUTHINFO = 15;
+
+    /** NNTP command code {@value}. */
     public static final int XOVER = 16;
+
+    /** NNTP command code {@value}. */
     public static final int XHDR = 17;
 
     private static final String[] commands = { "ARTICLE", "BODY", "GROUP", "HEAD", "HELP", "IHAVE", "LAST", "LIST", "NEWGROUPS", "NEWNEWS", "NEXT", "POST",
             "QUIT", "SLAVE", "STAT", "AUTHINFO", "XOVER", "XHDR" };
 
     /**
-     * Retrieve the NNTP protocol command string corresponding to a specified command code.
-     * <p>
+     * Gets the NNTP protocol command string corresponding to a specified command code.
      *
      * @param command The command code.
      * @return The NNTP protcol command string corresponding to a specified command code.
