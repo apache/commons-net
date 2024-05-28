84a96495af7ca72611236755a80671d6817c582c
Gary Gregory <garydgregory@gmail.com>
2024-05-28 10:25:51 -0400

    Add tests

---
+++ src/test/java/org/apache/commons/net/daytime/DaytimeUDPClientTest.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.net.daytime;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.net.InetAddress;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests {@link DaytimeUDPClient}.
+ */
+public class DaytimeUDPClientTest {
+
+    @SuppressWarnings("resource")
+    @Test
+    public void testConstructor() {
+        assertDoesNotThrow(DaytimeUDPClient::new);
+    }
+
+    @Test
+    public void testClose() {
+        try (DaytimeUDPClient client = new DaytimeUDPClient()) {
+            // empty
+        }
+    }
+
+    @Test
+    public void testGetTime() {
+        try (DaytimeUDPClient client = new DaytimeUDPClient()) {
+            // Not connected
+            assertThrows(NullPointerException.class, () -> client.getTime(InetAddress.getLocalHost()));
+            assertThrows(NullPointerException.class, () -> client.getTime(InetAddress.getLocalHost(), 0));
+        }
+    }
+}
