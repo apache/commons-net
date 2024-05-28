daaa5ac312dff42d0432372b431acf81efba5e18
Gary Gregory <garydgregory@gmail.com>
2024-05-28 08:10:48 -0400

    Add tests

--- src/test/java/org/apache/commons/net/util/TrustManagerUtilsTest.java
+++ src/test/java/org/apache/commons/net/util/TrustManagerUtilsTest.java
@@ -1,6 +1,11 @@
 package org.apache.commons.net.util;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +13,21 @@ import org.junit.jupiter.api.Test;
  * Tests {@link TrustManagerUtils}.
  */
 public class TrustManagerUtilsTest {
+
+    @Test
+    public void testGetAcceptAllTrustManager() {
+        assertNotNull(TrustManagerUtils.getAcceptAllTrustManager());
+    }
+
+    @Test
+    public void testGetDefaultTrustManager() throws KeyStoreException, GeneralSecurityException {
+        assertNotNull(TrustManagerUtils.getDefaultTrustManager(KeyStore.getInstance(KeyStore.getDefaultType())));
+    }
+
+    @Test
+    public void testGetValidateServerCertificateTrustManager() {
+        assertNotNull(TrustManagerUtils.getValidateServerCertificateTrustManager());
+    }
 
     @SuppressWarnings("deprecation")
     @Test
