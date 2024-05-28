a61ee76562614ef62428d9cc9de4662f4c371895
Gary Gregory <garydgregory@gmail.com>
2024-05-28 10:51:31 -0400

    Add tests

---
+++ src/test/java/org/apache/commons/net/bsd/RLoginClientTest.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.net.bsd;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests {@link RLoginClient}.
+ */
+public class RLoginClientTest {
+
+    private RLoginClient newClient() {
+        return new RLoginClient();
+    }
+
+    @Test
+    public void testConstructor() {
+        assertDoesNotThrow(RLoginClient::new);
+    }
+
+    @Test
+    public void testDefaultPort() {
+        assertEquals(RLoginClient.DEFAULT_PORT, newClient().getDefaultPort());
+    }
+
+    @Test
+    public void testGetInputStream() throws IOException {
+        try (InputStream inputStream = newClient().getInputStream()) {
+            assertNull(inputStream);
+        }
+    }
+
+}
