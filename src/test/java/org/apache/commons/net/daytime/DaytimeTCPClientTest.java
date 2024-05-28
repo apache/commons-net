ff5bc6bc9bd0fc28138f69d4cd40dac2b7a9275d
Gary Gregory <garydgregory@gmail.com>
2024-05-28 09:44:16 -0400

    Normalize test class visiblity

--- src/test/java/org/apache/commons/net/daytime/DaytimeTCPClientTest.java
+++ src/test/java/org/apache/commons/net/daytime/DaytimeTCPClientTest.java
@@ -38,7 +38,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-class DaytimeTCPClientTest {
+public class DaytimeTCPClientTest {
 
     private static MockDaytimeTCPServer mockDaytimeTCPServer;
 
