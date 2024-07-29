99e0c5c7b056a6d8b7a8c377489b301c8418979c
Gary Gregory <garydgregory@gmail.com>
2024-07-29 07:34:20 -0400

    Use Java code tag

--- src/main/java/org/apache/commons/net/ftp/parser/NTFTPEntryParser.java
+++ src/main/java/org/apache/commons/net/ftp/parser/NTFTPEntryParser.java
@@ -49,7 +49,7 @@ public class NTFTPEntryParser extends ConfigurableFTPFileEntryParserImpl {
      * The sole constructor for an NTFTPEntryParser object.
      *
      * @throws IllegalArgumentException Thrown if the regular expression is unparseable. Should not be seen under normal conditions. If it is seen, this is a
-     *                                  sign that <code>REGEX</code> is not a valid regular expression.
+     *                                  sign that {@code REGEX} is not a valid regular expression.
      */
     public NTFTPEntryParser() {
         this(null);
@@ -60,7 +60,7 @@ public class NTFTPEntryParser extends ConfigurableFTPFileEntryParserImpl {
      *
      * @param config The {@link FTPClientConfig configuration} object used to configure this parser.
      * @throws IllegalArgumentException Thrown if the regular expression is unparseable. Should not be seen under normal conditions. If it is seen, this is a
-     *                                  sign that <code>REGEX</code> is not a valid regular expression.
+     *                                  sign that {@code REGEX} is not a valid regular expression.
      * @since 1.4
      */
     public NTFTPEntryParser(final FTPClientConfig config) {
@@ -83,8 +83,8 @@ public class NTFTPEntryParser extends ConfigurableFTPFileEntryParserImpl {
     }
 
     /**
-     * Parses a line of an NT FTP server file listing and converts it into a usable format in the form of an <code>FTPFile</code> instance. If the file
-     * listing line doesn't describe a file, <code>null</code> is returned, otherwise a <code>FTPFile</code> instance representing the files in the
+     * Parses a line of an NT FTP server file listing and converts it into a usable format in the form of an {@code FTPFile} instance. If the file
+     * listing line doesn't describe a file, {@code null} is returned, otherwise a {@code FTPFile} instance representing the files in the
      * directory is returned.
      *
      * @param entry A line of text from the file listing
