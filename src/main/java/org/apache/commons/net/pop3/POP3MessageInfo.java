2a8f8761c7ad0dd486c6072c7b078fc8ef1dd365
Gary Gregory <garydgregory@gmail.com>
2024-07-29 07:34:39 -0400

    Use Java code tag

--- src/main/java/org/apache/commons/net/pop3/POP3MessageInfo.java
+++ src/main/java/org/apache/commons/net/pop3/POP3MessageInfo.java
@@ -21,13 +21,13 @@ package org.apache.commons.net.pop3;
  * POP3MessageInfo is used to return information about messages stored on a POP3 server. Its fields are used to mean slightly different things depending on the
  * information being returned.
  * <p>
- * In response to a status command, <code>number</code> contains the number of messages in the mailbox, <code>size</code> contains the size of the mailbox
- * in bytes, and <code>identifier</code> is null.
+ * In response to a status command, {@code number} contains the number of messages in the mailbox, {@code size} contains the size of the mailbox
+ * in bytes, and {@code identifier} is null.
  * <p>
- * In response to a message listings, <code>number</code> contains the message number, <code>size</code> contains the size of the message in bytes, and
- * <code>identifier</code> is null.
+ * In response to a message listings, {@code number} contains the message number, {@code size} contains the size of the message in bytes, and
+ * {@code identifier} is null.
  * <p>
- * In response to unique identifier listings, <code>number</code> contains the message number, <code>size</code> is undefined, and <code>identifier</code>
+ * In response to unique identifier listings, {@code number} contains the message number, {@code size} is undefined, and {@code identifier}
  * contains the message's unique identifier.
  */
 
@@ -37,15 +37,15 @@ public final class POP3MessageInfo {
     public String identifier;
 
     /**
-     * Creates a POP3MessageInfo instance with <code>number</code> and <code>size</code> set to 0, and <code>identifier</code> set to null.
+     * Creates a POP3MessageInfo instance with {@code number} and {@code size} set to 0, and {@code identifier} set to null.
      */
     public POP3MessageInfo() {
         this(0, null, 0);
     }
 
     /**
-     * Creates a POP3MessageInfo instance with <code>number</code> set to <code>num</code>, <code>size</code> set to <code>octets</code>, and
-     * <code>identifier</code> set to null.
+     * Creates a POP3MessageInfo instance with {@code number} set to {@code num}, {@code size} set to {@code octets}, and
+     * {@code identifier} set to null.
      *
      * @param num    the number
      * @param octets the size
@@ -55,8 +55,8 @@ public final class POP3MessageInfo {
     }
 
     /**
-     * Creates a POP3MessageInfo instance with <code>number</code> set to <code>num</code>, <code>size</code> undefined, and <code>identifier</code> set to
-     * <code>uid</code>.
+     * Creates a POP3MessageInfo instance with {@code number} set to {@code num}, {@code size} undefined, and {@code identifier} set to
+     * {@code uid}.
      *
      * @param num the number
      * @param uid the UID
