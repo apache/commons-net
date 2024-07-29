194afd410653759d4fc1ed6f4257bd2208c840a8
Gary Gregory <garydgregory@gmail.com>
2024-07-29 07:34:10 -0400

    Use Java code tag

--- src/main/java/org/apache/commons/net/ftp/FTPFileEntryParser.java
+++ src/main/java/org/apache/commons/net/ftp/FTPFileEntryParser.java
@@ -29,7 +29,7 @@ import java.util.List;
  * Here are some examples showing how to use one of the classes that implement this interface.
  * <p>
  *
- * The first example uses the <code>FTPClient.listFiles()</code> API to pull the whole list from the subfolder <code>subfolder</code> in one call, attempting to
+ * The first example uses the {@code FTPClient.listFiles()} API to pull the whole list from the subfolder {@code subfolder} in one call, attempting to
  * automatically detect the parser type. This method, without a parserKey parameter, indicates that autodection should be used.
  *
  * <pre>
@@ -39,7 +39,7 @@ import java.util.List;
  * FTPFile[] files = f.listFiles("subfolder");
  * </pre>
  *
- * The second example uses the <code>FTPClient.listFiles()</code> API to pull the whole list from the current working directory in one call, but specifying by
+ * The second example uses the {@code FTPClient.listFiles()} API to pull the whole list from the current working directory in one call, but specifying by
  * class name the parser to be used. For this particular parser class, this approach is necessary since there is no way to autodetect this server type.
  *
  * <pre>
@@ -49,7 +49,7 @@ import java.util.List;
  * FTPFile[] files = f.listFiles("org.apache.commons.net.ftp.parser.EnterpriseUnixFTPFileEntryParser", ".");
  * </pre>
  *
- * The third example uses the <code>FTPClient.listFiles()</code> API to pull a single file listing in an arbitrary directory in one call, specifying by KEY the
+ * The third example uses the {@code FTPClient.listFiles()} API to pull a single file listing in an arbitrary directory in one call, specifying by KEY the
  * parser to be used, in this case, VMS.
  *
  * <pre>
@@ -66,8 +66,8 @@ import java.util.List;
  */
 public interface FTPFileEntryParser {
     /**
-     * Parses a line of an FTP server file listing and converts it into a usable format in the form of an <code>FTPFile</code> instance. If the file listing
-     * line doesn't describe a file, <code>null</code> should be returned, otherwise a <code>FTPFile</code> instance representing the files in the directory
+     * Parses a line of an FTP server file listing and converts it into a usable format in the form of an {@code FTPFile} instance. If the file listing
+     * line doesn't describe a file, {@code null} should be returned, otherwise a {@code FTPFile} instance representing the files in the directory
      * is returned.
      *
      * @param listEntry A line of text from the file listing
