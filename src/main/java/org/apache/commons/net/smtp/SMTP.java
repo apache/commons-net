8edcbb0b2b59ed04562a2135012d3237b16bd644
Gary Gregory <garydgregory@gmail.com>
2024-07-29 07:34:41 -0400

    Use Java code tag

--- src/main/java/org/apache/commons/net/smtp/SMTP.java
+++ src/main/java/org/apache/commons/net/smtp/SMTP.java
@@ -41,8 +41,8 @@ import org.apache.commons.net.util.NetConstants;
  * You should keep in mind that the SMTP server may choose to prematurely close a connection for various reasons. The SMTP class will detect a premature SMTP
  * server connection closing when it receives a {@link org.apache.commons.net.smtp.SMTPReply#SERVICE_NOT_AVAILABLE SMTPReply.SERVICE_NOT_AVAILABLE } response to
  * a command. When that occurs, the SMTP class method encountering that reply will throw an {@link org.apache.commons.net.smtp.SMTPConnectionClosedException} .
- * <code>SMTPConnectionClosedException</code> is a subclass of <code>IOException</code> and therefore need not be caught separately, but if you are going to
- * catch it separately, its catch block must appear before the more general <code>IOException</code> catch block. When you encounter an
+ * {@code SMTPConnectionClosedException} is a subclass of {@code IOException} and therefore need not be caught separately, but if you are going to
+ * catch it separately, its catch block must appear before the more general {@code IOException} catch block. When you encounter an
  * {@link org.apache.commons.net.smtp.SMTPConnectionClosedException} , you must disconnect the connection with
  * {@link org.apache.commons.net.SocketClient#disconnect disconnect() } to properly clean up the system resources used by SMTP. Before disconnecting, you may
  * check the last reply code and text with {@link #getReplyCode getReplyCode }, {@link #getReplyString getReplyString }, and {@link #getReplyStrings
@@ -90,7 +90,7 @@ public class SMTP extends SocketClient {
     private String replyString;
 
     /**
-     * The default SMTP constructor. Sets the default port to <code>DEFAULT_PORT</code> and initializes internal data structures for saving SMTP reply
+     * The default SMTP constructor. Sets the default port to {@code DEFAULT_PORT} and initializes internal data structures for saving SMTP reply
      * information.
      */
     public SMTP() {
@@ -241,7 +241,7 @@ public class SMTP extends SocketClient {
 
     /**
      * Returns the integer value of the reply code of the last SMTP reply. You will usually only use this method after you connect to the SMTP server to check
-     * that the connection was successful since <code>connect</code> is of type void.
+     * that the connection was successful since {@code connect} is of type void.
      *
      * @return The integer value of the reply code of the last SMTP reply.
      */
