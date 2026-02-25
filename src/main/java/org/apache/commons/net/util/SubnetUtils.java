1b44a40feaa9388fb42d928e698c9e7156de2045
Gary Gregory <garydgregory@gmail.com>
2026-02-25 08:29:10 -0500

    Fix Javadoc @see tag

--- src/main/java/org/apache/commons/net/util/SubnetUtils.java
+++ src/main/java/org/apache/commons/net/util/SubnetUtils.java
@@ -25,7 +25,7 @@ import java.util.stream.StreamSupport;
 /**
  * Performs subnet calculations given a network address and a subnet mask.
  *
- * @see "https://datatracker.ietf.org/doc/html/rfc1519"
+ * @see <a href="https://datatracker.ietf.org/doc/html/rfc1519">Classless Inter-Domain Routing (CIDR): an Address Assignment and Aggregation Strategy</a>
  * @since 2.0
  */
 public class SubnetUtils {
