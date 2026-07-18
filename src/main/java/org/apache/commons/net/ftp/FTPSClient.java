a79729464f6e69b773171ff30e3d483cab7afd70
Javid Khan <dxbjavid@gmail.com>
2026-07-18 23:20:22 +0530

    Run hostname verifier on FTPS data connection (#404)
    * run hostname verifier on FTPS data connection
    
    * Update FTPSClient.java
    
    ---------
    
    Co-authored-by: Gary Gregory <garydgregory@users.noreply.github.com>

--- src/main/java/org/apache/commons/net/ftp/FTPSClient.java
+++ src/main/java/org/apache/commons/net/ftp/FTPSClient.java
@@ -299,6 +299,9 @@ public class FTPSClient extends FTPClient {
                 sslSocket.setEnabledProtocols(protocols);
             }
             sslSocket.startHandshake();
+            if (isClientMode && hostnameVerifier != null && !hostnameVerifier.verify(_hostname_, sslSocket.getSession())) {
+                throw new SSLHandshakeException("Hostname doesn't match certificate");
+            }
         }
 
         return socket;
